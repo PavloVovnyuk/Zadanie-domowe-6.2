d83d3a35deabbafd2b49b6351d18eaa452f5a834
Pavlo <pawel.wowniuk@gmail.com>
2019-01-26 20:39:34 +0100

    Zadanie domowe 6.2

---
+++ src/Array.java
@@ -0,0 +1,6 @@
+public class Array {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5,};
+        System.out.println ("Suma 1, 3, i 5 liczb równa się " + (array[0] + array[2] + array[4]));
+    }
+}
